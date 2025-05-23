092578f8896194f1e4a470d73aaba843640e0fdf
njayman <najishm828282@gmail.com>
2025-05-23 23:43:15 +0600

    fixed build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Affliates from "@/components/layout/Affliates";
 import Contact from "@/components/layout/Contact";
-import Footer from "@/components/layout/Footer";
-import Header from "@/components/layout/Header";
 import Hero from "@/components/layout/Hero";
 import Sectors from "@/components/layout/Sectors";
 
